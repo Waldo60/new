5f25956680f92b1675c21730aea28b4de637d6bf
Waldo60 <80797901+Waldo60@users.noreply.github.com>
2022-01-09 20:03:29 -0400

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-          touch $HOME/fim
-         [[ -e $HOME/fim ]] && rm $HOME/fim && break
-         echo -e "${col5}"
-         sleep 1s
 echo -e "\033[1;31m———————————————————————————————————————————————————\033[1;37m"
 echo -e "\033[1;32m              WEBSOCKET SSH "
 echo -e "\033[1;31m———————————————————————————————————————————————————\033[1;37m"
