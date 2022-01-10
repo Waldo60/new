6196b9d755859605937ff12fd86af99d13156b83
Waldo60 <80797901+Waldo60@users.noreply.github.com>
2022-01-09 20:19:42 -0400

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -3,6 +3,6 @@
           touch $HOME/fim
          [[ -e $HOME/fim ]] && breakrm $HOME/fim && 
 echo -e "\033[1;31m———————————————————————————————————————————————————\033[1;37m"
-echo -e "\033[1;32m              WEBSOCKET SSH "
+echo -e "\033[1;32m              Woli"
 echo -e "\033[1;31m———————————————————————————————————————————————————\033[1;37m"
 echo -e "\033[1;37m      WEBSOCKET SSH USARA A PORTA 80 e 44
