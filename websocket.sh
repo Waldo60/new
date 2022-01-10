9c1d2f37d8e47a638efebbe1d731081b80e771cb
Waldo60 <80797901+Waldo60@users.noreply.github.com>
2022-01-09 20:02:11 -0400

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
-fun_bar () {
-          comando[0]="$1"
-          comando[1]="$2"
-          (
-          [[ -e $HOME/fim ]] && rm $HOME/fim
-          ${comando[0]} > /dev/null 2>&1
-          ${comando[1]} > /dev/null 2>&1
           touch $HOME/fim
-          ) > /dev/null 2>&1 &
-          tput civis
-		  echo -e "\033[1;31m---------------------------------------------------\033[1;37m"
-          echo -ne "${col7}    AGUARDE..\033[1;35m["
-          while true; do
-          for((i=0; i<18; i++)); do
-          echo -ne "\033[1;34m#"
-          sleep 0.2s
-          done
          [[ -e $HOME/fim ]] && rm $HOME/fim && break
          echo -e "${col5}"
          sleep 1s
